87e7b0747f51e50999f755eba2f1e1df52447e39
J <djefre@users.noreply.github.com>
2024-09-04 14:06:14 +0200

    Update remove-avatarcontainer.user.js

--- roll20/remove-avatarcontainer.user.js
+++ roll20/remove-avatarcontainer.user.js
@@ -1,1 +1,16 @@
+// ==UserScript==
+// @name         Remove player avatars bottom-left
+// @namespace    https://github.com/djefre/tampermonkey-scripts/
+// @version      0.1
+// @description  Removes the player avatars at the bottom left of the screen
+// @author       djefre
+// @match        https://app.roll20.net/editor/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=roll20.net
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    
+    document.getElementById('avatarContainer').remove()
+})();
