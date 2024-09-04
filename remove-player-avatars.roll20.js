bcf7193d43a7735fad1e5f0068a1a0725f234250
J <djefre@users.noreply.github.com>
2024-09-04 14:02:23 +0200

    Update remove-player-avatars.roll20.js

--- remove-player-avatars.roll20.js
+++ remove-player-avatars.roll20.js
@@ -3,7 +3,7 @@
 // @namespace    djefre.roll20
 // @version      0.1
 // @description  Removes the player avatars at the bottom left of the screen
-// @author       You
+// @author       djefre
 // @match        https://app.roll20.net/editor/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=roll20.net
 // @grant        none
