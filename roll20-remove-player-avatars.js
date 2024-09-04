02ccd157f46e9c82821e4ac1c03e2887ddd16de7
J <djefre@users.noreply.github.com>
2024-09-04 13:22:45 +0200

    Create roll20-remove-player-avatars.js

---
+++ roll20-remove-player-avatars.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Remove player avatars bottom-left
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Removes the player avatars at the bottom left of the screen
+// @author       You
+// @match        https://app.roll20.net/editor/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=roll20.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById('avatarContainer').remove()
+    // Your code here...
+})();
